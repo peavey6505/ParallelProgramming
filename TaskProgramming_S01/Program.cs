b0273414f1bc758627961760f8d50e4b15239501
TomaszLenart <t.lenart234@gmail.com>
2026-02-18 23:22:16 +0100

    Waiting for tasks

--- TaskProgramming_S01/Program.cs
+++ TaskProgramming_S01/Program.cs
@@ -1,11 +1,12 @@
-﻿namespace TaskProgramming_S01
+﻿using System.Net.NetworkInformation;
+
+namespace TaskProgramming_S01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
-            
+          
         }
 
         static void RunExamples() {
@@ -13,8 +14,44 @@
             CancellationTokenSourceExample();
             CompositeCancellationTokensExample();
             WaitForTimeToPassExample();
+            WaitingForTaskExample();
         }
 
+        static void WaitingForTaskExample()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+
+
+            var t = new Task(() =>
+            {
+                Console.WriteLine("I take 5 secs");
+
+                for (int i = 0; i < 5; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"{i + 1} sec");
+                }
+                Console.WriteLine("Im done");
+
+            }, token);
+            t.Start();
+
+
+            Task t2 = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(3000);
+            }, token);
+
+            //Task.WaitAll(t, t2); // waits for all tasks to complete or be cancelled
+            //Task.WaitAny(t, t2); // waits for any task to complete or be cancelled
+            Task.WaitAll(new[] { t, t2 }, 4000, token); // waits for all tasks to complete or be cancelled, with a timeout
+
+            Console.WriteLine($"Task t status: {t.Status}");
+            Console.WriteLine($"Task t2 status: {t2.Status}");
+        }
         static void WaitForTimeToPassExample()
         {
             var cts = new CancellationTokenSource();
